743f7c42b1b33d3944105d4b0e52b1ecd96d482f
WelehoBRUDER <61124548+WelehoBRUDER@users.noreply.github.com>
2025-04-23 17:27:01 +0300

    Added JSDocs to button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,10 @@
+/**
+ * A reusable button component. Pass props and children as normal.
+ *
+ * @param {CallableFunction} onClick - callback function that triggers when the button is clicked
+ * @param {string} className - additional class names for styling (default: "btn")
+ * @returns
+ */
 const Button = ({onClick, className, icon, children}) => {
   return (
     <button onClick={onClick} className={className ? `${className}` : "btn"}>
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -5,7 +5,6 @@ const Home = () => {
     <div>
       <h1>Dice&Dine</h1>
       <p>Welcome to the homepage.</p>
-      <Button onClick={() => alert("Button clicked!")}>Start</Button>
     </div>
   );
 };
