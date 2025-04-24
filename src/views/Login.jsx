f575659ee8267259e14b9d4f669667cb9c1b9f53
leevi-rinnetmaki <leevi.rinnetmaki@hotmail.com>
2025-04-24 18:48:42 +0300

    This commit os broken. Attempting to merge to home-page after this commit. Working on localisation.

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,6 +1,12 @@
 import Form from "../components/Form";
+import {useLanguage} from "../context/LanguageContext";
+import {useEffect} from "react";
 
 const Login = () => {
+  const {lang, setCurrentPage} = useLanguage();
+  useEffect(() => {
+    setCurrentPage("login_page");
+  }, []);
   return <Form type="login"></Form>;
 };
 
