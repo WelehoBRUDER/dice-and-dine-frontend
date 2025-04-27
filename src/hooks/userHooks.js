ddfc475b42dd7fb429c8ba25380d4fb40eeb863d
nealukumies <nea.lukumies@metropolia.fi>
2025-04-27 09:40:42 +0300

    Add UserContext and authentication hooks; refactor App and Form components for user login functionality

---
+++ src/hooks/userHooks.js
@@ -0,0 +1,50 @@
+import {fetchData} from "../utils/fetchData";
+const apiURL = import.meta.env.VITE_API_URL;
+
+const useAuthentication = () => {
+  const postLogin = async (inputs) => {
+    const fetchOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(inputs),
+    };
+    const loginResult = await fetchData(apiURL + "/auth/login", fetchOptions);
+    console.log("loginresult", loginResult);
+
+    return loginResult;
+  };
+
+  return {postLogin};
+};
+
+const useUser = () => {
+  const postUser = async (inputs) => {
+    const fetchOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(inputs),
+    };
+    const registerResult = await fetchData(apiURL + "/users", fetchOptions);
+    console.log("Post user results:  ", registerResult);
+    return registerResult;
+  };
+
+  const getUserByToken = async (token) => {
+    const fetchOptions = {
+      headers: {
+        Authorization: "Bearer: " + token,
+      },
+    };
+    const userResult = await fetchData(apiURL + "/auth/me", fetchOptions);
+    console.log("userResult: ", userResult);
+    return userResult;
+  };
+
+  return {getUserByToken, postUser};
+};
+
+export {useAuthentication, useUser};
