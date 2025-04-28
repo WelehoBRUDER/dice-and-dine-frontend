10de92e4093fb84d4dc7f93d475b06aec8c09f8c
leevi-rinnetmaki <leevi.rinnetmaki@hotmail.com>
2025-04-28 14:18:07 +0300

    Working on about page

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -1,8 +1,23 @@
+import {useEffect} from "react";
+import {useLanguage} from "../context/LanguageContext";
+
 const About = () => {
+  const {lang, setCurrentPage} = useLanguage();
+  useEffect(() => {
+    setCurrentPage("about_page");
+  }, []);
+
   return (
-    <div>
-      <h1>About page</h1>
-      <p>This is the about page.</p>
+    <div id="about-page">
+      <h1>Contact information</h1>
+      <ul>
+        <li>{String.fromCodePoint(0x260e) + lang("id")} +1012 3456 789</li>
+        <li>{String.fromCodePoint(0x2709)} demo@gmail.com</li>
+        <li>
+          {String.fromCodePoint(0x1f4cc)} 132 Darthmouth Street Boston,
+          Massachusetts 02156 United States
+        </li>
+      </ul>
     </div>
   );
 };
