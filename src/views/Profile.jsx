98561ff45db804ce958993a2791c5c8da386de71
nealukumies <nea.lukumies@metropolia.fi>
2025-04-28 15:06:26 +0300

    Implement user details fetching in Profile component and update UI to display user information

--- src/views/Profile.jsx
+++ src/views/Profile.jsx
@@ -1,18 +1,38 @@
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import {useLanguage} from "../context/LanguageContext";
 import {useUserContext} from "../hooks/useUserContext";
+import {useUser} from "../hooks/userHooks";
+import Loading from "../components/Loading";
 
 const Profile = () => {
   const {user} = useUserContext();
+  const {getUserDetails} = useUser();
   const {lang, setCurrentPage} = useLanguage();
+  const [userDetails, setUserDetails] = useState(null);
   useEffect(() => {
     setCurrentPage("profile_page");
   }, []);
   console.log("user in profile: ", user);
+  useEffect(() => {
+    if (!user?.id) return;
+    const fetchUserDetails = async () => {
+      const data = await getUserDetails(user.id);
+      setUserDetails(data);
+    };
+
+    fetchUserDetails();
+  }, []);
+
+  if (!userDetails) {
+    return <Loading />;
+  }
+
   return (
     <div>
       <h1>{lang("profile_page.title")}</h1>
-      <p>This is the profile page.</p>
+      <p>Username: {userDetails.name}</p>
+      <p>E-mail: {userDetails.email}</p>
+      <p>User type: {userDetails.user_type}</p>
     </div>
   );
 };
