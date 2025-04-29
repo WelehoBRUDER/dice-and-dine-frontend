d80d4bf1af74a89b09b30c1d5147d4367d03bbed
WelehoBRUDER <61124548+WelehoBRUDER@users.noreply.github.com>
2025-04-29 11:38:52 +0300

    Improved style

--- src/components/LoadingWheel.jsx
+++ src/components/LoadingWheel.jsx
@@ -1,12 +1,19 @@
 import {useLanguage} from "../context/LanguageContext";
 import "../style/loading-wheel.css";
 
-const LoadingWheel = () => {
+/**
+ *
+ * @param {String} loadingText - The key of the text to display while loading. The default is "loading_text".
+ * @returns
+ */
+const LoadingWheel = ({loadingText = "loading_text"}) => {
   const {lang} = useLanguage();
   return (
     <div className="loading-wheel">
-      <div className="spinner"></div>
-      <span className="desc">{lang("loading_text")}</span>
+      <div className="spinner">
+        <div className="block"></div>
+      </div>
+      <span className="desc">{lang(loadingText)}</span>
     </div>
   );
 };
