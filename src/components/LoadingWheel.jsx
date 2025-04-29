13486fdf8746eaa0f94419b780b2a066ee1cd170
WelehoBRUDER <61124548+WelehoBRUDER@users.noreply.github.com>
2025-04-29 11:40:28 +0300

    Added more information to JSDocs

--- src/components/LoadingWheel.jsx
+++ src/components/LoadingWheel.jsx
@@ -3,9 +3,13 @@ import "../style/loading-wheel.css";
 
 /**
  *
+ * Simple loading wheel component with a spinner and a text description.
+ * Use this with conditional rendering to show or hide it based on the loading state of the app.
+ *
  * @param {String} loadingText - The key of the text to display while loading. The default is "loading_text".
  * @returns
  */
+
 const LoadingWheel = ({loadingText = "loading_text"}) => {
   const {lang} = useLanguage();
   return (
