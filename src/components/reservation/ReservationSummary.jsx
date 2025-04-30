5d7c758e20fbe88a243566abbcc346da78cff19e
WelehoBRUDER <61124548+WelehoBRUDER@users.noreply.github.com>
2025-04-30 14:06:12 +0300

    Post request is made but fails

--- src/components/reservation/ReservationSummary.jsx
+++ src/components/reservation/ReservationSummary.jsx
@@ -1,7 +1,10 @@
 import LoadingWheel from "../LoadingWheel";
 import ResultWindow from "../ResultWindow";
+import TextArea from "../TextArea";
 import Button from "../Button.jsx";
-import {useLanguage} from "../../context/LanguageContext.jsx";
+import {useLanguage} from "../../context/LanguageContext";
+import {useState} from "react";
+import useReservation from "../../hooks/useReservation";
 
 /**
  *
@@ -14,23 +17,100 @@ import {useLanguage} from "../../context/LanguageContext.jsx";
  */
 const ReservationSummary = ({date, arrival, length, tables, title}) => {
   const {lang} = useLanguage();
+  const {makeReservation, loading} = useReservation();
+  const [result, setResult] = useState(null);
+  const [message, setMessage] = useState("");
+
+  const canMakeReservation = () => {
+    return date && arrival && length && tables.length > 0;
+  };
+
+  const handleSubmit = () => {
+    const arrivalDate = new Date(date);
+    const [hours, minutes] = arrival.split(":").map(Number);
+    const arrivalHours = hours + minutes / 60;
+    arrivalDate.setHours(arrivalHours);
+    const departureDate = new Date(date);
+    departureDate.setHours(arrivalHours + length);
+
+    // These values will be sent to the backend
+    const values = {
+      arrival_time: arrivalDate,
+      departure_time: departureDate,
+      tables: tables,
+      additional_information: message,
+    };
+    const token = localStorage.getItem("token");
+
+    makeReservation(values, token)
+      .then((response) => {
+        console.log(response);
+        if (response?.success) {
+          setResult({
+            success: true,
+            title: "reservation_success",
+            desc: "reservation_success_desc",
+          });
+        } else {
+          setResult({
+            success: false,
+            title: "reservation_failed",
+            desc: "reservation_failed_desc",
+            tryAgainCallback: () => {
+              setResult(null);
+            },
+          });
+        }
+      })
+      .catch((error) => {
+        console.error("Error making reservation:", error);
+        setResult({
+          success: false,
+          title: "reservation_failed",
+          desc: "reservation_failed_desc",
+          tryAgainCallback: () => {
+            setResult(null);
+          },
+        });
+      });
+  };
   return (
-    <div className="summary">
+    <div className="summary scrollable">
       <h2>{title}</h2>
-      <div className="summary-details flex-column center">
-        <p>
-          {lang("date")}: {date.toLocaleDateString("fi-FI")}
-        </p>
-        <p>
-          {lang("arrival_time")}: {arrival}
-        </p>
-        <p>
-          {lang("length_of_stay")}: {length} h
-        </p>
-        <p>
-          {lang("tables")}: {tables.join(", ")}
-        </p>
-      </div>
+      {loading && <LoadingWheel loadingText="making_reservation" />}
+      {!loading && !result && (
+        <div className="summary-details flex-column">
+          <p>
+            {lang("date")}: {date.toLocaleDateString("fi-FI")}
+          </p>
+          <p>
+            {lang("arrival_time")}: {arrival}
+          </p>
+          <p>
+            {lang("length_of_stay")}: {length} h
+          </p>
+          <p>
+            {lang("tables")}: {tables.join(", ")}
+          </p>
+          <TextArea
+            name="info"
+            text={lang("additional_info")}
+            onChange={(e) => setMessage(e.target.value)}
+          ></TextArea>
+          <Button onClick={handleSubmit} disabled={!canMakeReservation()}>
+            {lang("make_reservation")}
+          </Button>
+        </div>
+      )}
+      {!loading && result && (
+        <ResultWindow
+          success={result.success}
+          title={lang(result.title)}
+          desc={lang(result.desc)}
+          continueCallback={result.continueCallback}
+          tryAgainCallback={result.tryAgainCallback}
+        />
+      )}
     </div>
   );
 };
---
+++ src/hooks/useReservation.js
@@ -0,0 +1,86 @@
+import {useState} from "react";
+import {fetchData} from "../utils/fetchData.js";
+import {useUserContext} from "./useUserContext.js";
+const API_URL = import.meta.env.VITE_API_URL;
+
+const useReservation = () => {
+  const [loading, setLoading] = useState(false);
+  const [reservationSuccess, setReservationSuccess] = useState(false);
+  const [reservationId, setReservationId] = useState(null);
+  const [error, setError] = useState(null);
+  const {user} = useUserContext();
+
+  const makeReservation = async (reservation, token) => {
+    setLoading(true);
+    setError(null);
+    setReservationSuccess(false);
+    console.log("cart user", user);
+
+    try {
+      const data = await fetchData(
+        `${API_URL}/tables/reserve/${user.username}`,
+        {
+          method: "POST",
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            reservation: reservation,
+          }),
+        }
+      );
+
+      if (data) {
+        setReservationSuccess(true);
+        setReservationId(data.reservation_id);
+      } else {
+        setError(data?.message || "Unknown error");
+      }
+    } catch (err) {
+      setError("Failed to place order: " + err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  // const getOrderDetails = async (orderId) => {
+  //   setLoading(true);
+  //   setError(null);
+
+  //   try {
+  //     const data = await fetchData(`${API_URL}/orders/${orderId}`, {
+  //       method: "GET",
+  //       headers: {
+  //         Authorization: `Bearer ${user.token}`,
+  //         "Content-Type": "application/json",
+  //       },
+  //     });
+
+  //     if (data) {
+  //       const ordersWithTime = data.order.map((orderItem) => ({
+  //         ...orderItem,
+  //         time: data.time,
+  //       }));
+
+  //       return ordersWithTime;
+  //     } else {
+  //       setError("Failed to fetch order details");
+  //     }
+  //   } catch (err) {
+  //     setError("Failed to fetch order details: " + err.message);
+  //   } finally {
+  //     setLoading(false);
+  //   }
+  // };
+
+  return {
+    makeReservation,
+    loading,
+    reservationSuccess,
+    reservationId,
+    error,
+  };
+};
+
+export default useReservation;
