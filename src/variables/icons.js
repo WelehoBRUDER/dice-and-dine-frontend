9240807a322aa6e4b4ad861c14abe773651a100e
WelehoBRUDER <61124548+WelehoBRUDER@users.noreply.github.com>
2025-05-02 10:47:01 +0300

    icons

--- src/variables/icons.js
+++ src/variables/icons.js
@@ -1,7 +1,21 @@
 import user from "../assets/icons/user.svg";
 import lock from "../assets/icons/lock.svg";
+import calendar from "../assets/icons/calendar.svg";
+import clock from "../assets/icons/schedule.svg";
+import map from "../assets/icons/map.svg";
+import calendar_clock from "../assets/icons/calendar_clock.svg";
+import overview from "../assets/icons/overview.svg";
+import star from "../assets/icons/star.svg";
+import star_filled from "../assets/icons/star_gold.svg";
 
 export const icons = {
   user,
   lock,
+  calendar,
+  clock,
+  map,
+  calendar_clock,
+  overview,
+  star,
+  star_filled,
 };
