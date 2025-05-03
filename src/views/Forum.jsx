5fb70b6141402cf3df11ade3c899d28ecf9915cd
leevi-rinnetmaki <leevi.rinnetmaki@hotmail.com>
2025-05-03 19:18:41 +0300

    Improved visuals of the forum page

--- src/views/Forum.jsx
+++ src/views/Forum.jsx
@@ -1,21 +1,27 @@
 import useForum from "../hooks/useForum";
 import {useLanguage} from "../context/LanguageContext";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import LoadingWheel from "../components/LoadingWheel";
+import Button from "../components/Button";
 
 const Forum = () => {
   const {lang, setCurrentPage} = useLanguage();
-
   const {forum, loading} = useForum(lang);
+
+  const [clickedItem, setClickedItem] = useState([]);
+  const handleItemClick = (item) => {
+    setClickedItem(item);
+  };
+
   console.log("forum", forum);
 
   useEffect(() => {
     setCurrentPage("forum_page");
   }, []);
   return (
-    <div>
+    <>
       <article>
-        <title>{lang("forum_title")}</title>
+        <title>{lang("forum_page_title")}</title>
         <meta name="description" content={lang("forum_description")} />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </article>
@@ -24,18 +30,32 @@ const Forum = () => {
         <LoadingWheel />
       ) : (
         <section id="forum-section">
-          {forum.map((item) => (
-            <article class="forum-article" key={item.id}>
-              <h2>1: {item?.id}</h2>
-              <h2>2: {item?.by_user_id}</h2>
-              <h2>3: {item?.to_message_id}</h2>
-              <h2>4: {item?.message_id}</h2>
-            </article>
-          ))}
+          {forum.map((item) =>
+            clickedItem?.id == item.id ? (
+              <article class="forum-article clicked-article" key={item.id}>
+                <Button
+                  className="btn-smaller"
+                  onClick={() => handleItemClick(null)}
+                >
+                  {String.fromCodePoint(0x274c)}
+                </Button>
+                <h2>title: {item?.id}</h2>
+                <p>preview: {item?.by_user_id}</p>
+              </article>
+            ) : (
+              <article
+                class="forum-article"
+                key={item.id}
+                onClick={() => handleItemClick(item)}
+              >
+                <h2>title: {item?.id}</h2>
+                <p>preview: {item?.by_user_id}</p>
+              </article>
+            )
+          )}
         </section>
       )}
-      <p>This is the forum page.</p>
-    </div>
+    </>
   );
 };
 
