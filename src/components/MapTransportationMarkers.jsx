7318fde712e42a84c4d8664f318cf6f18560f104
WelehoBRUDER <61124548+WelehoBRUDER@users.noreply.github.com>
2025-05-05 21:03:20 +0300

    Icons are visible

--- src/components/MapTransportationMarkers.jsx
+++ src/components/MapTransportationMarkers.jsx
@@ -2,6 +2,7 @@ import {useEffect, useState} from "react";
 import {Marker, Popup} from "react-leaflet";
 import ScooterIcon from "../assets/icons/scooter.svg";
 import BusIcon from "../assets/icons/bus.svg";
+import L from "leaflet";
 
 const MapTransportationMarkers = ({data}) => {
   const [transportationStops, setTransportationStops] = useState([]);
@@ -17,6 +18,26 @@ const MapTransportationMarkers = ({data}) => {
       setTransportationScooters(scooterNodes);
     }
   }, [data]);
+  const iconSize = [32, 32];
+  const iconAnchor = [16, 16];
+
+  const LBusIcon = new L.divIcon({
+    className: "",
+    html: `<div class="material-icon-wrapper blue"><img src=${BusIcon} class="material-icons"/></div>`,
+    iconSize: iconSize,
+    iconAnchor: iconAnchor,
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41],
+  });
+
+  const LScooterIcon = new L.divIcon({
+    className: "",
+    html: `<div class="material-icon-wrapper gold"><img src=${ScooterIcon} class="material-icons"/></div>`,
+    iconSize: iconSize,
+    iconAnchor: iconAnchor,
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41],
+  });
 
   return (
     <>
@@ -25,7 +46,7 @@ const MapTransportationMarkers = ({data}) => {
         const {lon, lat} = node.place;
         if (!lat || !lon) return null; // Skip if coords are not defined
         return (
-          <Marker key={node.distance} position={[lat, lon]} icon={BusIcon}>
+          <Marker key={node.distance} position={[lat, lon]} icon={LBusIcon}>
             <Popup>{node.place.name}</Popup>
           </Marker>
         );
@@ -35,7 +56,7 @@ const MapTransportationMarkers = ({data}) => {
         const {lon, lat} = node.place;
         if (!lat || !lon) return null; // Skip if coords are not defined
         return (
-          <Marker key={node.distance} position={[lat, lon]} icon={ScooterIcon}>
+          <Marker key={node.distance} position={[lat, lon]} icon={LScooterIcon}>
             <Popup>{node.place.name}</Popup>
           </Marker>
         );
