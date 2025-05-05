cd76055541f835175003abf3b38302b9a16c4732
WelehoBRUDER <61124548+WelehoBRUDER@users.noreply.github.com>
2025-05-05 15:08:37 +0300

    Added map invalidator to ensure proper rendering

--- src/components/LeafletMap.jsx
+++ src/components/LeafletMap.jsx
@@ -3,15 +3,17 @@ import {MapContainer, TileLayer, Marker, Popup} from "react-leaflet";
 import {useLanguage} from "../context/LanguageContext";
 import useRestaurantInfo from "../hooks/useRestaurantInfo";
 import LoadingWheel from "./LoadingWheel";
+import MapInvalidator from "./MapInvalidator";
 
 const LeafletMap = ({children}) => {
-  const [position, setPosition] = useState([61.471446, 23.788578]); // Default position (latitude, longitude)
+  const [position, setPosition] = useState(null); // Default position (latitude, longitude)
   const {lang} = useLanguage();
   const {info, loading} = useRestaurantInfo("en"); // Fetch restaurant info in English
 
   useEffect(() => {
     if (info.length > 0) {
       const {latitude, longitude} = info[0];
+      console.log(latitude, longitude);
       setPosition([latitude, longitude]); // Update position with fetched coordinates
     }
   }, [info]);
@@ -19,10 +21,10 @@ const LeafletMap = ({children}) => {
   return (
     <div className="map-embed flex-column ">
       {children}
-      {loading ? (
+      {loading || !position ? (
         <LoadingWheel />
       ) : (
-        <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+        <MapContainer center={position} zoom={13} scrollWheelZoom={true}>
           <TileLayer
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -30,6 +32,7 @@ const LeafletMap = ({children}) => {
           <Marker position={position}>
             <Popup>{lang("restaurant_title")}</Popup>
           </Marker>
+          <MapInvalidator position={position} />
         </MapContainer>
       )}
     </div>
